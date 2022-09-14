a4240f352cfd9d57fc630d9486f312270fc8401c
bienheureuseuwineza <85676961+Aime78@users.noreply.github.com>
2022-09-14 08:18:38 +0200

    practice ended.

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,5 +1,13 @@
+import { useCoffeeBean } from "../context/CoffeeContext";
+
 const SelectedCoffeeBean = () => {
-  return null;
-}
+  const { coffeeBean } = useCoffeeBean();
+  // console.log(coffeeBean);
+  return (
+    <div className="selected-coffee">
+      <h2>{coffeeBean.name}</h2>
+    </div>
+  );
+};
 
 export default SelectedCoffeeBean;
--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,21 +1,24 @@
+import { useState } from "react";
+import { useCoffeeBean } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { coffeeBean, setCoffeeBeanId } = useCoffeeBean();
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        value={coffeeBean.id}
+        onChange={(e) => setCoffeeBeanId(e.target.value)}
       >
-        {coffeeBeans.map(bean => (
-          <option
-            key={bean.id}
-            value={bean.id}
-          >
+        {coffeeBeans.map((bean) => (
+          <option key={bean.id} value={bean.id}>
             {bean.name}
           </option>
         ))}
       </select>
     </div>
   );
-}
+};
 
 export default SetCoffeeBean;
